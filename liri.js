c2867d373a77fc56e8e38d80c051a04bc9917a9b
creativelaurels <lauren@creativelaurels.com>
2016-08-25 16:15:31 -0400

    Created movie function

--- liri.js
+++ liri.js
@@ -1,5 +1,3 @@
-// twitter keys variable
-var keys = require('./keys.js')
 
 // declaring command variables
 var action = process.argv[2];
@@ -28,23 +26,26 @@ switch(action){
     break;
 }
 
-// TWITTER FUNCTION
+// TWITTER FUNCTIONnode liri.
 function twitter(){
-
+  // twitter keys variable, referencing the keys file and export line
+  var twitterKeys = require('./keys.js').twitterKeys;
+  // npm package
   var Twitter = require('twitter');
-
-  var client = new Twitter({
-    consumer_key: 'GX9Tln4JRsbV98Jg3Sae0j9ya',
-    consumer_secret_key: 'vBYXjcxpoeCkC6gk04TRVq9HK3LBPCL8ZWKD4ucz4G28apX8F4',
-    access_token: '745949227264598016-NweQfD11EvWlGAfARQ7W2UQbrfqBIlQ',
-    access_token_secret: 'l1wLgYrlNVF26Wt5xmVkOw54I6V0tNgBxX2TPBBh3s9lL'
-  });
-
+  // assigning the keys
+  var client = new Twitter(twitterKeys);
+  // what to search for
   var params = {screen_name: 'creativelaurels'};
+  console.log(params);
 
+  // using the npm
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    console.log(tweets);
-    if (!error) {
+  console.log("Here");
+    //if error, log it, else log the tweets
+    if (error) {
+      console.log(error);
+    }
+    else {
       console.log(tweets);
     }
   });
@@ -53,14 +54,51 @@ function twitter(){
 
 //SPOTIFY FUNCTION
 function spotify() {
-
+  //npm package
   var spotify = require('spotify');
-  spotify.search({ type: 'track', query: value }, function(err, data) {
+
+  spotify.search({type: 'track', query: value, }, function(err, data) {
     if ( err ) {
         console.log('Error occurred: ' + err);
         return;
     }
 
+    console.log("/////////Data////////")
     console.log(data);
+    console.log("///////Data.tracks///////")
+    var spotifyCall = data.tracks;
+    console.log(spotifyCall);
+
 });
+}
+
+//OMDB FUNCTION
+function movie() {
+  //npm package
+var request = require('request');
+// set movie name equal to user input
+var movieName = value;
+// search url variable
+var url = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&r=json';
+
+
+    request(url, function (error, resonse,body) {
+
+      console.log(body);
+      // If the request is successful (i.e. if the response status code is 200)
+      if (!error && response.statusCode == 200) {
+
+        // Parse the body of the site and recover just the imdbRating
+        // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+        console.log("The movie's rating is: " + JSON.parse(body)["imdbRating"])
+      }
+
+          // if user doesn't enter a value value will be set to Mr.Nobody
+          else {
+
+          }  
+      });
+
+
+    })
 }
